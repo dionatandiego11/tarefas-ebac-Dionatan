2afc41cf7048b0495d11bbf793847a89ee036779
Dionatan Resende <dionatanderesende@gmail.com>
2023-12-19 15:35:21 -0300

    atualizando o arquivo

--- Tarefa_Wrappers/Wrappers.java
+++ Tarefa_Wrappers/Wrappers.java
@@ -1,27 +1,27 @@
 import java.util.Scanner;
 
 public class Wrappers {
-    static long valor;
-    static int valorS;
-    static Scanner scanner = new Scanner(System.in);
+    static int valor; // valor primitivo
+    static Integer valorS; // valor como objeto Integer
+    static Scanner scanner = new Scanner(System.in); // scanner para ler a entrada
 
     public static void main(String args[]) {
-        scanner();
-        conversao();
-        resultado();
+        scanner(); // lê o valor
+        conversao(); // converte o valor para Integer
+        resultado(); // imprime o valor e o tipo
     }
 
     public static void scanner() {
-        System.out.println("Digite o primeiro valor: ");
-        valor = scanner.nextLong();
+        System.out.println("Digite o valor: ");
+        valor = scanner.nextInt(); // lê o valor
     }
     
     public static void conversao() {
-       valorS = (int) valor;
+       valorS = Integer.valueOf(valor); // converte o valor para Integer
     }
 
     public static void resultado() {
-        System.out.println("O resultado da conversão é: " + valorS);
+        System.out.println(valorS); // imprime o valor
+        System.out.println("Tipo do valor convertido: " + valorS.getClass().getSimpleName()); // imprime o tipo do valor convertido
     }
 }
-
