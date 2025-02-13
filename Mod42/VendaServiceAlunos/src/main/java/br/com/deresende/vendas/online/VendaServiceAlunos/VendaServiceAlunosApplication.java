6f9a356993c478b64d75add3cc5bc1888de76fb4
Dionatan Resende <dionatan2003@gmail.com>
2025-02-13 15:12:45 -0300

    Mod42

---
+++ Mod42/VendaServiceAlunos/src/main/java/br/com/deresende/vendas/online/VendaServiceAlunos/VendaServiceAlunosApplication.java
@@ -0,0 +1,13 @@
+package br.com.deresende.vendas.online.VendaServiceAlunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaServiceAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaServiceAlunosApplication.class, args);
+	}
+
+}
